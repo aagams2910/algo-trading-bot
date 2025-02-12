9b287c79fca04ea39789fdf5b4e6aa155afd6437
AAGAM <aagams2910@gmail.com>
2025-02-12 16:52:12 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import alpaca_trade_api as tradeapi
 from backtrader import Cerebro, feeds, Strategy
 from datetime import datetime, timedelta
 import time
+
+API_KEY = 'your_api_key'
+SECRET_KEY = 'your_secret_key'
+BASE_URL = 'https://paper-api.markets' 
+
