be5b99fd197cf8dd914edbfc741b302616b644b7
AAGAM <aagams2910@gmail.com>
2025-02-12 16:59:32 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
 import pandas_ta as ta
-import alpaca_trade_api as tradeapi
+from kiteconnect import KiteConnect
 from backtrader import Cerebro, feeds, Strategy
 from datetime import datetime, timedelta
 import time
 
+# Zerodha Kite API credentials
 API_KEY = 'your_api_key'
-SECRET_KEY = 'your_secret_key'
-BASE_URL = 'https://paper-api.markets' 
-
+ACCESS_TOKEN = 'your_access_token'
