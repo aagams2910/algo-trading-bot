38c3dfbd8a51223d0c3339e1b517f739450f6c5c
AAGAM <aagams2910@gmail.com>
2025-02-13 20:04:48 +0530

    updated main.py

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ ACCESS_TOKEN = 'your_access_token'
 
 kite = KiteConnect(api_key=API_KEY)
 kite.set_access_token(ACCESS_TOKEN)
+
+#Fetch historical data
+def fetch_historical_data(symbol, start_date, end_date, timeframe='day'):
+    data = kite.historical_data(instrument_token=symbol, from_date=start_date, to_date=end_date, interval=timeframe)
+    data = pd.DataFrame(data)
+    data['date'] = pd.to_datetime(data['date'])
+    data.set_index('date', inplace=True)
+    return data
